99aa292d8052447160b4548861b3d8c27b9bdb06
Jose Flores <jop0693@gmail.com>
2024-01-15 13:47:23 -0600

    last update

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,8 +4,15 @@ vim.g.mapleader = " "
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- remove hightlight of searched world
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+
+-- diagnostic float text
 vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float)
+
+-- quickfix list navigation
+vim.keymap.set("n", "<leader>]", "<cmd>cnext<CR>zz", { desc = "Forward qfixlist" })
+vim.keymap.set("n", "<leader>[", "<cmd>cprev<CR>zz", { desc = "Backward qfixlist" })
 
 -- others
 vim.wo.number = true
@@ -16,4 +23,4 @@ vim.opt.softtabstop = 4
 vim.opt.tabstop = 4
 vim.opt.expandtab = true
 vim.opt.scrolloff = 8
-
+vim.opt.cindent = true
