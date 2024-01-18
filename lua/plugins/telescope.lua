e0e529d14880545011359c13bae03bf01d72bad7
Jose Flores <jop0693@gmail.com>
2024-01-18 11:18:07 -0600

    telescope configuration changes

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,38 +1,56 @@
 return {
-    {
-        "nvim-telescope/telescope-ui-select.nvim",
-    },
-    {
-        "nvim-telescope/telescope.nvim",
-        tag = "0.1.5",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        config = function()
-            require("telescope").setup({
-                defaults = {
-                    mappings = {
-                        i = {
-                            ["<C-h>"] = "which_key",
-                            ["<C-d>"] = require("telescope.actions").delete_buffer,
-                        },
-                        n = {
-                            ["<C-h>"] = "which_key",
-                            ["<C-d>"] = require("telescope.actions").delete_buffer,
-                        },
-                    },
-                },
-                extensions = {
-                    ["ui-select"] = {
-                        require("telescope.themes").get_dropdown({}),
-                    },
-                },
-            })
-            local builtin = require("telescope.builtin")
-            vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-            vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-            vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-            vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("telescope").setup({
+				defaults = {
+					vimgrep_arguments = {
+						"rg",
+						"--color=never",
+						"--no-heading",
+						"--with-filename",
+						"--line-number",
+						"--column",
+						"--smart-case",
+						"--hidden",
+					},
+					file_ignore_patterns = {
+						"node_modules",
+						".git",
+						"dist",
+						"yarn.lock",
+						"package%-lock.json",
+					},
+					mappings = {
+						i = {
+							["<C-h>"] = "which_key",
+							["<C-d>"] = require("telescope.actions").delete_buffer,
+						},
+						n = {
+							["<C-h>"] = "which_key",
+							["<C-d>"] = require("telescope.actions").delete_buffer,
+						},
+					},
+				},
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>fa", "<Cmd>Telescope find_files find_command=rg,--ignore,--hidden,--files prompt_prefix=🔍<CR>", {})
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 
-        require("telescope").load_extension("ui-select")
-        end,
-    },
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
