2459ad4d050793f6b1e75994884c520750814be2
Jose Flores <jop0693@gmail.com>
2024-09-03 20:05:08 -0600

    new color for copilot

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -19,7 +19,9 @@ vim.keymap.set("i", "<C-J>", 'copilot#Accept("\\<CR>")', {
 	expr = true,
 	replace_keycodes = false,
 })
+
 vim.g.copilot_no_tab_map = true
+vim.api.nvim_set_hl(0, "CopilotSuggestion", { fg = "#B2B1A2" })
 
 -- others
 vim.wo.number = true
